cbc73e6c55c407f791d7d39f7d276775f21cdfe3
QCoding <quats111@gmail.com>
2019-10-12 01:23:57 -0400

    iterate on package construction

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 from setuptools import setup, find_packages
 
+MAJOR = 0
+MINOR = 0
+PATCH = 0
+VERSION = '%d.%d.%d' % (MAJOR, MINOR, PATCH)
+
+packages = find_packages()
+
 setup(
     name="qsum",
-    version="0.0.0",
-    packages=find_packages(),
+    version=VERSION,
+    packages=packages,
     url="https://github.com/QCoding/qsum",
     description="Intuitive and extendable checksumming for python objects",
 )
