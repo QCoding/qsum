ba20be30744234d355a94d75ce78d969cf2a1a4d
QCoding <quats111@gmail.com>
2019-10-12 00:17:51 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="qsum",
+    version="0.0.0",
+    packages=find_packages(),
+    url="https://github.com/QCoding/qsum",
+    description="Intuitive and extendable checksumming for python objects",
+)
