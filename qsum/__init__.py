705eedc87d537c9fdd1df52a0b3f5be57b7a72a4
QCoding <quats111@gmail.com>
2019-10-12 16:22:18 -0400

    Add version and improve test logic (#45)
    * make git operations a little cleaner
    
    * all tests use _tester.py

--- qsum/__init__.py
+++ qsum/__init__.py
@@ -1,3 +1,10 @@
+import pkg_resources
+
 from qsum.core import exceptions
 from qsum.core.logic import checksum, Checksum, is_supported_type
 from qsum.util._tester import test
+
+try:
+    __version__ = pkg_resources.get_distribution('qsum').version
+except pkg_resources.DistributionNotFound:
+    __version__ = "unknown"
--- qsum/tests/test_structure.py
+++ qsum/tests/test_structure.py
@@ -1,3 +1,4 @@
+import qsum
 from qsum.core.constants import CONTAINER_TYPES
 from qsum.data.data_logic import all_data_types
 from qsum.types.type_logic import all_prefix_types
@@ -6,3 +7,8 @@ from qsum.types.type_logic import all_prefix_types
 def test_prefix_and_data_support():
     "Validate prefix type logic and data support the same set of types"
     assert set(all_data_types()).union(CONTAINER_TYPES) == set(all_prefix_types())
+
+
+def test_version():
+    """Verify we can compute a version"""
+    assert len(qsum.__version__) > 0
