b4b8ef8ee747e877123b28d2f8719700175d067f
QCoding <quats111@gmail.com>
2019-10-12 11:12:07 -0400

    clean up lint issues and add pkg to command

--- qsum/util/_tester.py
+++ qsum/util/_tester.py
@@ -1,5 +1,7 @@
+# pylint: disable=import-outside-toplevel
 """
 Entrypoint for testing from the top-level namespace
+# pattern is from https://github.com/pandas-dev/pandas/blob/master/pandas/util/_tester.py
 """
 import os
 import sys
@@ -7,16 +9,24 @@ import sys
 PKG = os.path.dirname(os.path.dirname(__file__))
 
 
-def test(extra_args=None):
+def test(extra_args=None) -> None:
+    """
+    Run the test suite for qsum
+    Args:
+        extra_args: additional args to pass to pytest
+
+    Returns: None
+    """
     try:
         import pytest
     except ImportError:
         raise ImportError("Need pytest to run tests")
-    cmd = []
+    cmd = []  # type: list
     if extra_args:
         if not isinstance(extra_args, list):
             extra_args = [extra_args]
         cmd = extra_args
+    cmd += [PKG]
     print("running: pytest {}".format(" ".join(cmd)))
     sys.exit(pytest.main(cmd))
 
--- setup.py
+++ setup.py
@@ -5,12 +5,24 @@ MINOR = 0
 PATCH = 0
 VERSION = '%d.%d.%d' % (MAJOR, MINOR, PATCH)
 
-packages = find_packages()
 
-setup(
-    name="qsum",
-    version=VERSION,
-    packages=packages,
-    url="https://github.com/QCoding/qsum",
-    description="Intuitive and extendable checksumming for python objects",
-)
+def setup_package():
+    """Perform the setup for qsum"""
+    packages = find_packages()
+
+    metadata = dict(
+        name="qsum",
+        author="Justin M. Quartiere",
+        license='MIT',
+        version=VERSION,
+        packages=packages,
+        url="https://github.com/QCoding/qsum",
+        description="Intuitive and extendable checksumming for python objects",
+        python_requires='>=3.5',
+    )
+
+    setup(**metadata)
+
+
+if __name__ == "__main__":
+    setup_package()
