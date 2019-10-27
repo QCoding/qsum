9e3a424e0895e4c49ef08dab5ee298877c9a17b9
QCoding <quats111@gmail.com>
2019-10-27 13:40:24 -0400

    add checksum_hex test and import

--- qsum/__init__.py
+++ qsum/__init__.py
@@ -1,7 +1,7 @@
 import pkg_resources
 
 from qsum.core import exceptions
-from qsum.core.logic import checksum, Checksum, is_supported_type
+from qsum.core.logic import checksum, checksum_hex, Checksum, is_supported_type
 from qsum.util._tester import test
 
 try:
