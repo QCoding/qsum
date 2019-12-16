a80eaa68585f79f30e80e9d2a01247a812014b2e
QCoding <quats111@gmail.com>
2019-12-15 23:59:30 -0500

    example of custom container type

---
+++ qsum/examples/inherited_container.py
@@ -0,0 +1,15 @@
+# pylint: disable=missing-function-docstring,missing-class-docstring
+from qsum import Checksum
+
+
+class CustomDict(dict):
+    def sorted_keys(self):
+        return sorted(self.keys())
+
+
+if __name__ == "__main__":
+    my_custom_dict = CustomDict()
+    my_custom_dict['b'] = 1
+    my_custom_dict['a'] = 2
+
+    print(Checksum(my_custom_dict).hex())
