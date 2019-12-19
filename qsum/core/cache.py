3c21416fc57873b340e868d98b7149ad028b46c5
QCoding <quats111@gmail.com>
2019-12-18 21:53:28 -0500

    fix spacing

--- qsum/core/cache.py
+++ qsum/core/cache.py
@@ -1,4 +1,4 @@
 from functools import lru_cache
 
 # the maxsize here should be ~ # of common types * # of groupings used (~3)
-is_sub_class = lru_cache(maxsize=256)(issubclass) # pylint: disable=invalid-name
+is_sub_class = lru_cache(maxsize=256)(issubclass)  # pylint: disable=invalid-name
