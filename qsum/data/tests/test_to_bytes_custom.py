87c0d9bd769b3779602f6df80bd465ae321ded37
QCoding <quats111@gmail.com>
2019-12-20 11:37:39 -0500

    clean up test

--- qsum/data/tests/test_to_bytes_custom.py
+++ qsum/data/tests/test_to_bytes_custom.py
@@ -1,8 +1,10 @@
+# pylint: disable=wildcard-import,unused-wildcard-import,redefined-outer-name
 from qsum.data.to_bytes_custom import int_to_bytes
 # noinspection PyUnresolvedReferences
 from qsum.tests.helpers import *
 
 
-def test_integer_conversion(range_2_16):
-    all_byte_values = list(map(repr, range_2_16))
+def test_integer_conversion_to_bytes(range_2_16):
+    """Verify integers get unique byte strings"""
+    all_byte_values = list(map(int_to_bytes, range_2_16))
     assert len(all_byte_values) == len(set(all_byte_values)), "Every integer should have a unique byte value"
