fc09ff21dd7aa0be5dc88d86bdc159241c6184d6
QCoding <quats111@gmail.com>
2020-01-09 11:47:07 -0500

    Pip cleanup (#79)
    * add setup.cfg and bump patch version
    
    * Add more classifiers
    
    * add newline
    
    * simply description
    
    * fix description references
    
    * change build back to 0

--- setup.py
+++ setup.py
@@ -1,16 +1,9 @@
-from os import path
-
 from setuptools import setup, find_packages
 
 MAJOR = 0
 MINOR = 2
-PATCH = 0
+PATCH = 1
 VERSION = '%d.%d.%d' % (MAJOR, MINOR, PATCH)
-
-# https://packaging.python.org/guides/making-a-pypi-friendly-readme/
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
 
 
 def setup_package():
@@ -25,12 +18,10 @@ def setup_package():
         packages=packages,
         url="https://github.com/QCoding/qsum",
         dev_url="https://github.com/QCoding/qsum",
-        summary="Python Checksumming Library",
         # in pkg-info this maps to 'summary'
-        description="Intuitive and extendable checksumming for python objects",
+        description="Python Checksumming Library",
         # in pkg-info this maps to 'description'
-        long_description=long_description,
-        long_description_content_type='text/markdown',
+        long_description="Intuitive and extendable checksumming for python objects",
         python_requires='>=3.5',
         keywords="checksum checksumming hashing",
         tests_require=['pytest'],
@@ -43,6 +34,9 @@ def setup_package():
             ]
         },
         classifiers=[
+            "Operating System :: OS Independent",
+            "Programming Language :: Python",
+            "Programming Language :: Python :: 3",
             "Programming Language :: Python :: 3.5",
             "Programming Language :: Python :: 3.6",
             "Programming Language :: Python :: 3.7",
